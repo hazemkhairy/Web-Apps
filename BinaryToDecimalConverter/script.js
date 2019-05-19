c5ad08ae6cd54213face8852de4243d6a441eea4
hazemkhairy <hazemkhairy188@gmail.com>
2019-05-19 07:16:14 +0200

    added Binary to decimal converter App still need CSS

---
+++ BinaryToDecimalConverter/script.js
@@ -0,0 +1,54 @@
+
+let button = document.getElementById("submitButton");
+let textBox = document.getElementById("inputTextBox");
+let resultDisplay = document.getElementById("resultDisplay");
+/*
+console.log(textBox);
+const onInputChange =()=>{
+    console.log("in");
+    
+    console.log(value);
+}
+
+*/
+
+const checkIfValid=(input)=>{
+    console.log(`now we check ${input}`);
+        
+    for(let i = 0 ; i < input.length ; i++)
+    {
+        console.log(`now we check ${input[i]}`);
+        if(!(input[i]<='1'&&input[i]>='0'))
+            return false ;
+    }
+    return true;
+}
+const transferNewInput=()=>{
+    let textBoxValue=textBox.value;
+    let toBeDisplayed="";
+    textBoxValue.trim();
+
+    if(!checkIfValid(textBoxValue))
+        toBeDisplayed="Please Enter Valid combination of 0's and 1's";
+    else
+    {
+        toBeDisplayed="Decimal = " + convertBinaryToDecimal(textBoxValue);
+        //toBeDisplayed=textBoxValue;
+    }
+    
+    resultDisplay.innerHTML=toBeDisplayed;
+}
+
+
+const convertBinaryToDecimal= (input)=>{
+    let ret =0;
+    let index = 0 ;    
+    for (let i = input.length - 1 ; i >=0 ; i--)
+        {
+            ret += (Math.pow(2,index)*(input[i]-'0'));
+            index++; 
+        }
+    return String(ret);
+}
+
+textBox.addEventListener("input",transferNewInput);
